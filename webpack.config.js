2ce88116439d595ec2a1c31feb07308a59ff8812
dai <chamestica@gmail.com>
2021-06-28 05:38:11 +0900

    Set up public directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 const srcRelativePath = 'src'
+const publicRelativePath = 'public'
 const distRelativePath = 'dist'
 
 module.exports = {
@@ -16,5 +18,15 @@ module.exports = {
     path: path.resolve(__dirname, distRelativePath)
   },
 
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, publicRelativePath),
+          to: path.resolve(__dirname, distRelativePath)
+        }
+      ]
+    })
+  ]
 }
