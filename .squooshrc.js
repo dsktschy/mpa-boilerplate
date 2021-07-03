24fd2916c55b29e425943c3a8c7c364b1c865acd
dai <chamestica@gmail.com>
2021-07-04 02:55:16 +0900

    Optimize images by default

--- .squooshrc.js
+++ .squooshrc.js
@@ -2,25 +2,25 @@
 
 module.exports = {
   mozjpeg: {
-    quality: 100
+    quality: 75
   },
   webp: {
-    quality: 100
+    quality: 75
   },
   avif: {
     // [0 - 63]
     // https://github.com/GoogleChromeLabs/squoosh/blob/ff9dea465f33c1712b145153360aa0244116887d/codecs/avif/enc/avif_enc.cpp#L9
-    cqLevel: 0
+    cqLevel: 33
   },
   jxl: {
-    quality: 100
+    quality: 75
   },
   wp2: {
-    quality: 100
+    quality: 75
   },
   oxipng: {
     // [1 - 6]
     // https://www.vox.me.uk/post/2020/08/oxipng-and-pngout/#oxipng
-    level: 1
+    level: 2
   }
 }
