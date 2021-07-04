f6e30c6503557ca5f398265822f4e1c4a6d936a7
dai <chamestica@gmail.com>
2021-07-05 06:09:51 +0900

    Separate functions in webpack.config.js into lib directory

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,11 @@ const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const { ImagePool, encoders } = require('@squoosh/lib')
 const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin')
-const squooshConfig = require('./.squooshrc.js')
 const svgoConfig = require('./.svgorc.js')
+const { optimizeImage } = require('./lib/copy')
+const { createTemplateParameters } = require('./lib/html')
+const { createGenerate } = require('./lib/manifest')
 
 dotenv.config()
 
@@ -21,125 +22,7 @@ const publicRelativePath =
   process.env.WEBPACK_PUBLIC_RELATIVE_PATH?.replace(/\/$/, '') || 'public'
 const distRelativePath =
   process.env.WEBPACK_DIST_RELATIVE_PATH?.replace(/\/$/, '') || 'dist'
-const scriptManifestKeyPrefix = 'assets/scripts/'
-const stylesheetManifestKeyPrefix = 'assets/stylesheets/'
 
-/**
- * Create instance of html-webpack-plugin to set common options
- */
-const createHtmlWebpackPlugin = (template, filename, templateParameters = {}) =>
-  new HtmlWebpackPlugin({
-    template,
-    filename,
-    inject: false,
-    templateParameters: (compilation, assets, assetTags, options) => ({
-      // Default parameters
-      // https://github.com/jantimon/html-webpack-plugin/blob/0a6568d587a82d88fd3a0617234ca98d26a1e0a6/index.js#L1098
-      compilation,
-      webpackConfig: compilation.options,
-      htmlWebpackPlugin: {
-        tags: assetTags,
-        files: assets,
-        options
-      },
-
-      /**
-       * Create hashed assets path
-       */
-      h: (_path = '') => {
-        const rootRelative = _path.startsWith('/')
-        const relativePath = rootRelative ? _path.slice(1) : _path
-        if (!relativePath) {
-          // eslint-disable-next-line
-          console.error(`\`${_path}\` doesn't exist in manifest.json.`)
-          return ''
-        }
-        const { dir, name, ext } = path.parse(relativePath)
-        const hashedRelativePath = dir
-          ? `${dir}/${name}.${compilation.hash}${ext}`
-          : `${name}.${compilation.hash}${ext}`
-        // Non-hashed assets
-        if (!Object.keys(compilation.assets).includes(hashedRelativePath)) {
-          return _path
-        }
-        // Hashed assets
-        return (rootRelative ? '/' : '') + hashedRelativePath
-      },
-
-      // Custom parameters per page
-      ...templateParameters
-    })
-  })
-
-/**
- * Create instance of svg-spritemap-webpack-plugin to set common options
- */
-const createSVGSpritemapPlugin = (input, filename) =>
-  new SVGSpritemapPlugin(input, {
-    output: {
-      filename,
-      svgo: svgoConfig,
-      svg4everybody: legacyMode
-    }
-  })
-
-/**
- * Override generated manifest.json
- * Because keys of entry files are not relative path, but only filename
- */
-const optimizeManifests = (seed, files) => {
-  const manifests = {}
-  const scriptNameList = []
-  const stylesheetNameList = []
-  for (const entryKey of Object.keys(config.entry)) {
-    scriptNameList.push(`${entryKey}.js`)
-    stylesheetNameList.push(`${entryKey}.css`)
-  }
-  for (const file of files) {
-    let name = file.name
-    if (file.isChunk) {
-      if (scriptNameList.includes(file.name)) {
-        name = `${scriptManifestKeyPrefix}${file.name}`
-      } else if (stylesheetNameList.includes(file.name)) {
-        name = `${stylesheetManifestKeyPrefix}${file.name}`
-      }
-    }
-    manifests[name] = file.path
-  }
-  return manifests
-}
-
-/**
- * Optimize image by Squoosh
- * https://web.dev/introducing-libsquoosh/
- * https://github.com/webpack-contrib/image-minimizer-webpack-plugin/blob/2736c42319fc54f2701123957e7ad6ec5aaa600f/src/utils/squooshMinify.js#L57
- */
-const optimizeImage = async (content, absoluteFrom) => {
-  let encoder = ''
-  const ext = path.extname(absoluteFrom)
-  for (const key of Object.keys(encoders)) {
-    if (ext === `.${encoders[key].extension}`) encoder = key
-  }
-  if (!encoder) return content
-  const imagePool = new ImagePool()
-  const image = imagePool.ingestImage(content)
-  let encodedImage = null
-  try {
-    await image.encode(squooshConfig)
-    // imagePool must be closed after image.encode
-    await imagePool.close()
-    encodedImage = await image.encodedWith[encoder]
-  } catch (error) {
-    // eslint-disable-next-line
-    console.error(error)
-    return content
-  }
-  return Buffer.from(encodedImage.binary)
-}
-
-/**
- * Configuration object
- */
 const config = {
   entry: {
     index: path.resolve(__dirname, `${srcRelativePath}/assets/index.ts`),
@@ -248,35 +131,59 @@ const config = {
     // svg-spritemap-webpack-plugin occurs unexpected and complicated behavior
     // with copy-webpack-plugin. So output to temporary directory in src
     // https://github.com/cascornelissen/svg-spritemap-webpack-plugin/issues/157
-    createSVGSpritemapPlugin(
+    new SVGSpritemapPlugin(
       path.resolve(__dirname, `${srcRelativePath}/assets/sprites/index/*.svg`),
-      `../${srcRelativePath}/assets/sprites/_/index.svg`
+      {
+        output: {
+          filename: `../${srcRelativePath}/assets/sprites/_/index.svg`,
+          svgo: svgoConfig,
+          svg4everybody: legacyMode
+        }
+      }
     ),
-    createSVGSpritemapPlugin(
+    new SVGSpritemapPlugin(
       path.resolve(__dirname, `${srcRelativePath}/assets/sprites/foobar/*.svg`),
-      `../${srcRelativePath}/assets/sprites/_/foobar.svg`
+      {
+        output: {
+          filename: `../${srcRelativePath}/assets/sprites/_/foobar.svg`,
+          svgo: svgoConfig,
+          svg4everybody: legacyMode
+        }
+      }
     ),
 
-    createHtmlWebpackPlugin(
-      path.resolve(__dirname, `${srcRelativePath}/templates/index.ejs`),
-      'index.html'
-    ),
-    createHtmlWebpackPlugin(
-      path.resolve(__dirname, `${srcRelativePath}/templates/foobar.ejs`),
-      'foobar.html'
-    ),
+    new HtmlWebpackPlugin({
+      template: path.resolve(
+        __dirname,
+        `${srcRelativePath}/templates/index.ejs`
+      ),
+      filename: 'index.html',
+      inject: false,
+      templateParameters: createTemplateParameters({})
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(
+        __dirname,
+        `${srcRelativePath}/templates/foobar.ejs`
+      ),
+      filename: 'foobar.html',
+      inject: false,
+      templateParameters: createTemplateParameters({})
+    }),
 
     new MiniCssExtractPlugin({
       filename: 'assets/stylesheets/[name].[fullhash].css'
-    }),
-
-    new WebpackManifestPlugin({
-      generate: optimizeManifests,
-      // Avoid unexpected prefix to manifest values
-      // https://github.com/shellscape/webpack-manifest-plugin/issues/229
-      publicPath: ''
     })
   ]
 }
+
+config.plugins.push(
+  new WebpackManifestPlugin({
+    generate: createGenerate(config, 'assets/scripts/', 'assets/stylesheets/'),
+    // Avoid unexpected prefix to manifest values
+    // https://github.com/shellscape/webpack-manifest-plugin/issues/229
+    publicPath: ''
+  })
+)
 
 module.exports = config
